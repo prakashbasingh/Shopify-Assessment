bd7154466580086a957a5fdd18aaa644fc7df5db
Prakash Bahadur Singh <prakashbahadursingh2@gmail.com>
2021-03-31 22:16:41 -0700

    styling almost complete, functionality to update price based on selected item and selected quantity successful.

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,23 @@
 // Put your application javascript here
+
+// const onSelectChange = () => {};
+
+// this.productSingleObject = JSON.parse(
+//   document.getElementById("ProductJson-" + variantId)
+// );
+// var product = this.productSingleObject;
+
+// console.log(product);
+
+// var productJson = window.document.getElementById("ProductJson-");
+
+// console.log(productJson);
+//  {% unless product == empty %}
+//           {% assign variantId = variant.id%}
+//           <script type="application/json" id="ProductJson-{{variantId}}">
+//                   {{ product | json }}
+//                 </script>
+//           <script type="application/json" id="ModelJson-{{variantId}}">
+//                   {{ product.media | where: 'media_type', 'model' | json }}
+//                 </script>
+//         {% endunless %}
